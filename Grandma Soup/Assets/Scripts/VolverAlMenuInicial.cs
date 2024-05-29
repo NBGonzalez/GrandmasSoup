148e813ec710813244987877fc16de2d8e93afe0
Nestor Bermejillo Gonzalez <n.bermejillo.2021@alumnos.urjc.es>
2024-05-29 18:28:09 +0200

    Mas cosas

---
+++ Grandma Soup/Assets/Scripts/VolverAlMenuInicial.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VolverAlMenuInicial : MonoBehaviour
+{
+    public void VolverMenuInicial()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
